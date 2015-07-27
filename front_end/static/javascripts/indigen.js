d21cb5f40d1dae425fc36dde0c2679eba397e03b
starsdeep <starsdeep on ZJU>
2015-07-27 17:36:06 +0800

    make index.html work

---
+++ front_end/static/javascripts/indigen.js
@@ -0,0 +1,33 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('indigen', [
+            'indigen.config',
+            'indigen.routes',
+            'indigen.authentication'
+        ]);
+
+    angular
+        .module('indigen.config', []);
+
+    angular
+        .module('indigen.routes', ['ngRoute']);
+
+
+    angular
+        .module('indigen')
+        .run(run);
+
+    run.$inject = ['$http'];
+
+    /**
+     * @name run
+     * @desc Update xsrf $http headers to align with Django's defaults
+     */
+    function run($http) {
+        $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+        $http.defaults.xsrfCookieName = 'csrftoken';
+    }
+
+})();
