aaff8c4aaadfd36b557607a83fff8daf9f9222b2
fucusy <fucusy@gmail.com>
2015-08-04 15:53:37 +0800

    remove unique from field it will cause error when Meta.abstract is True

--- back_end/indigen/authentication/forms.py
+++ back_end/indigen/authentication/forms.py
@@ -5,18 +5,27 @@ from rest_framework.validators import UniqueValidator
 from authentication.models import User
 from django.db import models
 from django import forms
+from base_class.base import BaseInitWithDic
+from location.models import Country
 
 class RegisterModel(models.Model):
     class Meta:
         abstract = True
-    telephone = models.CharField(max_length=30, null=False,blank=False)
-    password = models.CharField(null=False,max_length=128, blank=False)
+
+    telephone = models.CharField(max_length=20, null=True)
+    nickname = models.CharField(max_length=20,null=True,blank=False,default="")
+    password = models.CharField(max_length=128,blank=False,default="")
+    country = models.CharField(max_length=30, default="", blank=False)
+
+
 
     def clean(self):
+        errors = {}
         telephone = self.telephone
         user = User.objects.filter(username=telephone)
         if len(user) > 0:
-            raise forms.ValidationError({"telephone": "telephone already exists, you can login directly"})
+            errors["telephone"] = "telephone already exists, you can login directly"
 
-
-
+        if self.country != "" and len(Country.objects.filter(name=self.country)) == 0:
+            errors["country"] = "county not found"
+        raise forms.ValidationError(errors)
