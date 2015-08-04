6b893260435abd4ffe2db5229e60ac83080848d2
fucusy <fucusy@gmail.com>
2015-08-04 18:08:55 +0800

    ...

--- back_end/indigen/location/views.py
+++ back_end/indigen/location/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from authentication.views import BaseModelView
+from location.models import Country
+from location.serializers import CountrySerializer
 # Create your views here.
+
+class CountryViewSet(BaseModelView):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
