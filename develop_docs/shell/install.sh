864dd6867ca402ddd77510068f0770b91a6233a1
fucusy <fucusy@gmail.com>
2015-08-07 10:52:04 +0800

    rename insatll.sh to install.sh

--- develop_docs/shell/install.sh
+++ develop_docs/shell/install.sh
@@ -3,7 +3,10 @@
 # change work directory to this file' dir
 BASEDIR=$(dirname $0)
 cd $BASEDIR
+echo "next you need input mysql root password"
 # drop the database and create a new database
 cat ../sql/create_database.sql | mysql -u root -p
 python ../../back_end/indigen/manage.py makemigrations
 python ../../back_end/indigen/manage.py migrate
+
+echo "you can use default user(username:username, password:password ) to login"
