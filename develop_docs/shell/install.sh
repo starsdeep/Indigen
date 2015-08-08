005bbb55296f48a7ec49452eb1ee60ffc033a20a
fucusy <fucusy@gmail.com>
2015-08-08 10:38:56 +0800

    update install, add admin account

--- develop_docs/shell/install.sh
+++ develop_docs/shell/install.sh
@@ -9,4 +9,4 @@ cat ../sql/create_database.sql | mysql -u root -p
 python ../../back_end/indigen/manage.py makemigrations
 python ../../back_end/indigen/manage.py migrate
 
-echo "you can use default user(username:username, password:password ) to login"
+echo "you can use default user(username:username, password:password ) to login, default admin(username:admin, password:password)"
